c08625a38d8e6d122f67320689c2c2a2fbe81e0e
dmason30 <danmason@spendology.com>
2020-06-17 12:19:31 +0100

    Initial commit

---
+++ assets/js/snippet.js
@@ -0,0 +1,2 @@
+
+!function(){var e=document.createElement("script");document.getElementsByTagName("body")[0].appendChild(e),e.setAttribute("src","https://widget.spendology.io/next/loader.min.js?"+(new Date).getTime())}();
